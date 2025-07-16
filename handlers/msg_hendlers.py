1c97f1292677dfb1613bb7ff7068600feb853d3e
Azizbek <azizbekqoshoqov0206@example.com>
2025-07-16 08:09:22 +0500

    yangilash

--- handlers/msg_hendlers.py
+++ handlers/msg_hendlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.types import Message
-
+from aiogram.filters import CommandStart
 
 msg_router = Router()
 
@@ -8,3 +8,12 @@ msg_router = Router()
 async def msg_start(message: Message):
     text = f"Siz yozgan xabar --> {message.text}"
     await message.answer(text)
+
+from aiogram.exceptions import TelegramForbiddenError
+
+@dp.message(CommandStart())
+async def msg_start(message: Message):
+    try:
+        await message.answer("Salom!")
+    except TelegramForbiddenError:
+        print(f"Foydalanuvchi botni bloklagan: {message.from_user.id}")
